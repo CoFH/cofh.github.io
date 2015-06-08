2da8e325be85516c6e03e28913efd64be8e56ae2
Tonius <tonbro96@gmail.com>
2015-06-08 20:17:04 +0200

    Activate analytics outside dev environments, added CNAME

--- assets/js/cofh.js
+++ assets/js/cofh.js
@@ -29,3 +29,14 @@ $(window).on('hashchange load', function() {
             }, 0);
     }
 });
+
+// Google Analytics
+if(window.location.hostname.search('localhost') < 0 && window.location.hostname.search('127.0.0.1') < 0) {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-62994156-1', 'auto');
+    ga('send', 'pageview');
+}
