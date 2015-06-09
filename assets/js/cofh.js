9f63fe4badf696266f9733be67df09f4b5021387
Tonius <tonbro96@gmail.com>
2015-06-09 21:52:25 +0200

    Added working search bar, auto-disable links to pages that don't exist yet

--- assets/js/cofh.js
+++ assets/js/cofh.js
@@ -1,4 +1,55 @@
+---
+layout: null
+---
+
+var pages = [
+{% for page in site.pages %}    { title: '{{ page.title }}', value: '{{ page.title }}', url: '{{ page.url }}'{% if page.mod and page.title != page.mod %}, mod: '{{ page.mod }}'{% endif %} }{% if forloop.last %}{% else %},{% endif %}
+{% endfor %}
+];
+
+var setScroll = function() {
+    var $anchor = $(window.location.hash);
+
+    if ($anchor.length > 0) {
+        $('html, body')
+            .stop()
+            .animate({
+                scrollTop: $anchor.offset().top - 50
+            }, 0);
+    }
+};
+$(window).on('hashchange load', setScroll);
+
+$.fn.enterPress = function(fn) {
+    return this.each(function() {
+        $(this).bind('enterPress', fn);
+        $(this).keyup(function(e) {
+            if (e.keyCode == 13) {
+                $(this).trigger("enterPress");
+            }
+        })
+    });
+ };
+
 $(function() {
+    $('.cofh-search').on('selectitem.uk.autocomplete', function(event, data) {
+        window.location.href = window.location.protocol + '//' + window.location.host + data.url;
+    });
+    $('.cofh-search > input').enterPress(function() {
+        if ($('.cofh-search .uk-dropdown').attr('aria-expanded') == 'false' || $('.cofh-search .uk-dropdown > ul > li.uk-active').length == 0) {
+            window.location.href = 'http://google.com/search?q=' + $('.cofh-search > input').val() + ' site:' + window.location.hostname;
+        }
+    });
+
+    $(".uk-container a[href^='/docs/']").each(function(i, anchor) {
+        $anchor = $(anchor);
+        for (var i = 0; i < pages.length; i++) {
+            if (pages[i].url.search($anchor.attr('href')) >= 0) return;
+        }
+        $anchor.removeAttr('href');
+        $anchor.addClass('uk-text-danger');
+    });
+
     var nicks = [
         'RedstoneFurnace',
         'PhytogenicInsolator',
@@ -16,18 +67,6 @@ $(function() {
 
     var banner = '/assets/images/banners/banner' + Math.floor(Math.random() * 7 + 1) + '.jpg';
     $('.cofh-banner').css('background-image', 'url(' + banner + ')');
-});
-
-$(window).on('hashchange load', function() {
-    var $anchor = $(':target');
-
-    if ($anchor.length > 0) {
-        $('html, body')
-            .stop()
-            .animate({
-                scrollTop: $anchor.offset().top - 50
-            }, 0);
-    }
 });
 
 // Google Analytics
